40078d79f4169e0568483eb897339035a8f5a1c2
marcelo-bonora <marcelo.bonora@bandtec.com.br>
2022-04-04 22:07:23 -0300

    att

--- banco.py
+++ banco.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 con = mysql.connector.connect(
-host='127.0.0.1',
-database='analise_algoritmos',
-user='aluno',
-password = 'sptech')
+host='localhost',
+database='ProjetoAlgas',
+user='root',
+password = 'urubu100')
 
 print(con)
 
@@ -12,5 +12,6 @@ print(con)
 # create table tblProjetoAlgas (
 # 	id int primary key auto_increment,
 #     tempo_seg decimal(18, 2),
-#     espaco_byte decimal(18, 2)
+#     espaco_byte decimal(18, 2),
+#     tipo_rage varchar(100)
 # )
